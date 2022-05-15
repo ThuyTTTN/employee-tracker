a20a34e434e6477030469ca50e824bb682f1a4ed
Thuy Nguyen <ttruong06@gmail.com>
2022-05-15 09:52:01 -0400

    add bonus functions

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function init() {
           "Add a role",
           "Add an employee",
           "Update an employee",
+          "Update a manager",
           "Done",
         ],
       },
@@ -50,6 +51,12 @@ function init() {
           break;
         case "Update an employee":
           updateEmployee();
+          break;
+        case "Update a manager":
+          updateManager();
+          break;
+        case "View employees by Manager":
+          viewByManager();
           break;
         default:
           process.exit();
@@ -206,54 +213,165 @@ function addEmployee() {
 //UPDATE AN EMPLOYEE ROLE - select an employee to update and their new role; updated to db
 function updateEmployee() {
   connection.query(
-    "SELECT employee.last_name, role.title FROM employee JOIN role ON employee.role_id = role.id;",
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title AS role_title, department.name AS department_name, CONCAT(e.first_name, ' ' ,e.last_name) AS reporting_manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id ORDER BY id;",
     function (err, res) {
-      console.log(res);
-      inquirer
-        .prompt([
-          {
-            type: "list",
-            name: "lastName",
-            message: "What is the employee's last name? ",
-            choices: function () {
-              var lastName = [];
-              for (let i = 0; i < res.length; i++) {
-                lastName.push(res[i].last_name);
-              }
-              return lastName;
-            },
-          },
-          {
-            type: "list",
-            name: "role",
-            message: "What is the employee's new role? ",
-            choices: selectRole(),
-          },
-        ])
-        .then(function (res) {
-          var roleId = selectRole().indexOf(res.role) + 1;
-          connection.query(
-            "UPDATE employee SET role_id = ? WHERE last_name = ?",
-            [roleId, res.lastName],
-            function (err) {
-              if (err) throw err;
-              console.table(res);
-              init();
-            }
-          );
-        });
+      console.log("CURRENT EMPLOYEES TABLE:");
+      console.table(res);
+      connection.query(
+        "SELECT role.id AS role_id, role.title AS role_title FROM role;",
+        function (err, res) {
+          console.log("ROLE TABLE:");
+          console.table(res);
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "employeeId",
+                message:
+                  "Please use the above CURRENT EMPLOYEES TABLE as a reference to enter the ID number of the employee you are updating (If employee's new role is now a Manager position, please write: null):",
+                validate: (idInput) => {
+                  if (idInput) {
+                    return true;
+                  } else {
+                    console.log("Please enter the employee's ID number!");
+                    return false;
+                  }
+                },
+              },
+              {
+                type: "input",
+                name: "newRole",
+                message:
+                  "Please choose a role_id number from the above ROLE TABLE that corresponds to the employee's new role title (i.e. If employee's new role is HR Manager, write 1):",
+                validate: (newRoleInput) => {
+                  if (newRoleInput) {
+                    return true;
+                  } else {
+                    console.log(
+                      "Please enter a role_id number from the above ROLE TABLE!"
+                    );
+                    return false;
+                  }
+                },
+              },
+              {
+                type: "input",
+                name: "newManager",
+                message:
+                  "Please use the above CURRENT EMPLOYEES TABLE as a reference to enter the new manager's ID number for the employee you are updating (If employee's new role is now a Manager position, please write: null):",
+                validate: (newManagerInput) => {
+                  if (newManagerInput) {
+                    return true;
+                  } else {
+                    console.log("Please enter the new manager's ID number!");
+                    return false;
+                  }
+                },
+              },
+            ])
+            .then(function (val) {
+              connection.query(
+                "UPDATE employee SET role_id = ?, manager_id = ? WHERE id = ?;",
+                [val.newRole, JSON.parse(val.newManager), val.employeeId],
+                function (err) {
+                  if (err) throw err;
+                  console.table(val);
+                  console.log(
+                    `Employee with ID of ${val.employeeId} successfully updated!`
+                  );
+                  init();
+                }
+              );
+            });
+        }
+      );
     }
   );
 }
 
-// selectRole for the updateEmployee()
-var roleArr = [];
-function selectRole() {
-  connection.query("SELECT * FROM role", function (err, res) {
-    if (err) throw err;
-    for (var i = 0; i < res.length; i++) {
-      roleArr.push(res[i].title);
-    }
-  });
-  return roleArr;
+function updateManager() {
+  connection.query(
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title AS role_title, department.name AS department_name, CONCAT(e.first_name, ' ' ,e.last_name) AS reporting_manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id WHERE e.id IS NULL ORDER BY id;",
+    function (err, res) {
+      console.log('CURRENT MANAGERS TABLE:');
+      console.table(res);
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "employeeId",
+            message: "Please enter the ID number for the employee who needs their manager updated:",
+            validate: (idInput) => {
+              if (idInput) {
+                return true;
+              } else {
+                console.log("Please enter the employee\'s ID number!");
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'newManager',
+            message: "Please use the above CURRENT MANAGERS TABLE as a reference to enter the new manager\'s ID number for the employee you are updating (If employee\'s new role is now a Manager position, please write: null):",
+            validate: (newManagerInput) => {
+              if (newManagerInput) {
+                return true;
+              } else {
+                console.log("Please enter the new manager\'s ID number!");
+                return false;
+              }
+            }
+          },
+        ])
+        .then(function (val) {
+          connection.query(
+            'UPDATE employee SET manager_id = ? WHERE id = ?;',
+            [JSON.parse(val.newManager), val.employeeId],
+            function (err) {
+              if (err) throw err;
+              console.table(val);
+              console.log(`Manager for employee with ID of ${val.employeeId} successfully updated!`);
+              init();
+            }
+          );
+        });
+    });
+}
+
+function viewByManager() {
+  connection.query(
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title AS role_title, department.name AS department_name, CONCAT(e.first_name, ' ' ,e.last_name) AS reporting_manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id WHERE e.id IS NULL ORDER BY id;",
+    function (err, res) {
+      console.log('CURRENT MANAGERS TABLE:');
+      console.table(res);
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "viewByManager",
+            message: "Please use the above CURRENT MANAGERS TABLE as a reference to enter the manager\'s ID number (i.e. If you want to view all employees under Adaline Bowen (id = 4), write 4):",
+            validate: (managerInput) => {
+              if (managerInput) {
+                return true;
+              } else {
+                console.log("Please enter the manager\'s ID number!");
+                return false;
+              }
+            }
+          },
+        ])
+        .then(function (val) {
+          connection.query(
+            'SELECT * FROM employee WHERE manager_id = ?;',
+            [JSON.parse(val.viewByManager)],
+            function (err, res) {
+              if (err) throw err;
+              console.table(res);
+              console.log(`You selected to view all employees under the Manager with an ID of ${val.viewByManager}!`);
+              init();
+            }
+          );
+        });
+    })
 }
